8340f97175ddfa7dbbf3ec68e768f4271822a49e
xpure <xpure@foxmail.com>
2017-08-21 13:27:41 +0800

    f

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+import sys
+
+
+kw = dict(
+    name='single_process',
+    version="1.0",
+    description='limit python script just run a single process',
+    long_description=open('README', 'r').read(),
+    author='zsp',
+    author_email='xpure@foxmail.com',
+    py_modules=['single_process'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    url="https://github.com/xpurer/single_process"
+)
+
+if sys.version_info[1] == 5:
+    kw['install_requires'] = ['simplejson']
+
+setup(**kw)
---
+++ single_process.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import tempfile
+import fcntl
+import errno
+import signal
+from signal import SIGABRT,  SIGILL, SIGINT, SIGSEGV, SIGTERM, signal, SIGQUIT
+import atexit
+
+
+def signal_handler(signal, frame):
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGQUIT, signal_handler)
+    signal.signal(signal.SIGTSTP, signal_handler)
+
+
+def single_process():
+    filename = os.path.abspath(sys.argv[0])
+    lock_file_name = filename.lstrip('/').replace('/', '_') + '.lock'
+
+    lock_file_path = os.path.join(
+        tempfile.gettempdir(), lock_file_name)
+    fd = os.open(lock_file_path, os.O_CREAT | os.O_RDWR, 0o660)
+    try:
+        fcntl.lockf(fd, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError as e:
+        if e.errno in (errno.EACCES, errno.EAGAIN):
+            print(
+                filename, 'running. Quit'
+            )
+            sys.exit(0)
+        else:
+            raise
+
+    def exit(signal, frame):
+        os.close(fd)
+        os.remove(lock_file_path)
+        sys.exit(signal)
+
+    for sig in (SIGABRT,  SIGILL, SIGINT, SIGSEGV, SIGTERM, SIGQUIT):
+        signal(sig, exit)
+    atexit.register(lambda: exit(0, 0))
+
+
+if __name__ == "__main__":
+
+    single_process()
+
+    def test():
+        from time import sleep
+        from datetime import datetime
+        for i in range(3):
+            print(datetime.now())
+            sleep(1)
+    test()
